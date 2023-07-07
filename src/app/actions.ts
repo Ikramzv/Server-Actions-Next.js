b064405123873292d60049e7b258d98d0e6cc2bf
Ikram <=>
2023-07-08 00:20:36 +0400

    log returned todos

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { setTodos } from "./data";
 
 export async function addTodo(todo: string) {
-  setTodos(todo);
+  const todos = setTodos(todo);
   revalidatePath("/");
+  return todos;
 }
